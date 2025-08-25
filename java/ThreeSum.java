1ead864b311ffadbfbe374c6ae7961a802d64149
siddhi-pandey0913 <siddhipandey336699@gmail.com>
2025-08-25 20:29:40 +0530

    Solved Problems

---
+++ java/ThreeSum.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        Arrays.sort(arr);
+        Set<List<Integer>> result=new HashSet<>();
+        for(int i=0;i<arr.length-2;i++){
+            int left=i+1;
+            int right=arr.length-1;
+            while(left<right){
+                int sum=arr[i]+arr[left]+arr[right];
+                if(sum==0){
+                    result.add(Arrays.asList(arr[i],arr[left],arr[right]));
+                    left++;
+                    right--;
+                }
+                else if(sum<0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+} ThreeSum {
+    
+}
--- java/jumpGame.java
+++ java/jumpGame.java
@@ -1,4 +1,4 @@
-class Solution {
+class jumpGame {
     public boolean canJump(int[] arr) {
         int sum=0,maximum=0;
         for(int i=0;i<arr.length;i++){
